a2a6ba113be9b916e6beefa99d496a1bc4c17d32
Lustihgn <quangtrannhat41@gmail.com>
2026-02-14 09:44:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const stars = ["⭐","🌟","✨","💫","🌠","✦","✧"];
 
 const cards = [
   { img:"anh1.jpg", text:"❤️Chúc NThư năm mới sẽ có thêm thật là nhiều niềm vui ❤️" },
-  { img:"anh2.jpg", text:"Sau gặp lại đừng có nhìn t thế này nhá" },
+  { img:"anh2.jpg", text:"💀Sau gặp lại đừng có nhìn t thế này nhá👻" },
   { img:"anh1.jpg", text:"😍Chúc Thư sang năm mới sẽ ngày càng xinh đẹp hơn cả 😍" },
   { img:"anh3.jpg", text:"💔Có gặp lại đừng tỏ ra không quen biết nhe💔" },
   { img:"anh1.jpg", text:"💕Mong Thư sẽ luôn nhận được sự yêu thương và trân trọng 💕" },
